15dfcc2065cafdca18fd32aab61568962a28ce06
Samuel <samuel.venzi@me.com>
2020-07-23 08:28:16 -0300

    Toggle people

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -17,8 +17,14 @@ class App extends Component {
         { name: newName, age: "25" },
         { name: "Fernanda", age: "23" },
         { name: "Pedro", age: "18" },
-      ]
+      ],
+      show: "false"
     })
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.show
+    this.setState({show: !doesShow}) 
   }
 
   render() {
@@ -26,16 +32,21 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
         <h1>One more</h1>
-        <button onClick={() => this.switchNameHandler("Sam!")}>Switch name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}/>
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age} />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age} />
+        <button onClick={this.togglePersonsHandler}>Toggle</button>
+        {
+          this.state.show ?
+            <div>
+              <Person
+                name={this.state.persons[0].name}
+                age={this.state.persons[0].age} />
+              <Person
+                name={this.state.persons[1].name}
+                age={this.state.persons[1].age} />
+              <Person
+                name={this.state.persons[2].name}
+                age={this.state.persons[2].age} />
+            </div> : null
+        }
       </div>
     );
   }
